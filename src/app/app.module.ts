7f2a6e46720f5683538cbf19009a23fea06014d2
Camilo Saenz <rubiansaenz1997@gmail.com>
2022-03-16 17:08:27 -0500

    First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 
+import { SharedModule } from './shared/shared.module';
+import { GifsModule } from './gifs/gifs.module';
+import { FooterComponent } from './footer/footer.component';
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, FooterComponent],
+  imports: [BrowserModule, HttpClientModule, SharedModule, GifsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
